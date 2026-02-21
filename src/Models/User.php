1829139bc59bdc36747b2117d908c018cc5f0e09
firefirefile <withsofatv@gmail.com>
2026-02-21 15:16:49 +0500

    add User model with findByLogin method

---
+++ src/Models/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Models; 
+
+class User extends Model {
+    protected static string $table = 'users';
+    /**
+     * метод поиска по логину 
+     */
+    public static function findByLogin(string $login): ?array {
+
+        $sql = 'SELECT * FROM ' . static::$table . ' WHERE login = :login';
+        $stmt = self::$db->prepare($sql);
+        $stmt->execute(['login' => $login]);
+
+        return $stmt->fetch() ?: null;
+    }
+
+
+   
+
+};
