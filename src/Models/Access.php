939adb66b34f9fed02abcb768e28df9f060acec7
firefirefile <withsofatv@gmail.com>
2026-02-25 15:18:13 +0500

    change guest_id to the granted_to_id to fix errors

--- src/Models/Access.php
+++ src/Models/Access.php
@@ -9,9 +9,9 @@ class Access extends Model {
      * метод для предоставления доступа к библиотеке
      */
     public static function grant (int $ownerId, int $guestId):bool {
-        $sql = "SELECT id FROM " . static::$table . " 
-        WHERE owner_id = :owner_id 
-        AND guest_id = :guest_id";
+        $sql = "SELECT id FROM " . static::$table . "
+        WHERE owner_id = :owner_id
+        AND granted_to_id = :guest_id";
 
         $stmt = self::$db->prepare($sql);
         $stmt->execute([
@@ -25,16 +25,16 @@ class Access extends Model {
 
             return self::create([
             'owner_id' => $ownerId,
-            'guest_id' => $guestId
+            'granted_to_id' => $guestId
             ]) > 0;
     }
-    /** 
-     * метод для проверки, был ли выдан доступ к библиотеке 
+    /**
+     * метод для проверки, был ли выдан доступ к библиотеке
      */
     public static function hasAccess (int $ownerId, int $guestId):bool {
-        $sql = "SELECT id FROM " . static::$table . " 
-        WHERE owner_id = :owner_id 
-        AND guest_id = :guest_id";
+        $sql = "SELECT id FROM " . static::$table . "
+        WHERE owner_id = :owner_id
+        AND granted_to_id = :guest_id";
 
         $stmt = self::$db->prepare($sql);
         $stmt->execute([
@@ -45,15 +45,15 @@ class Access extends Model {
             return (bool) $stmt->fetch();
 
     }
-    /** 
+    /**
      * метод для запроса списка участников (для списка Кому я дал доступ к своей библиотеке)
      */
     public static function getGuests(int $ownerId):array {
-        $sql = 'SELECT u.id, u.login 
-            FROM users u 
-            JOIN access a ON u.id = a.guest_id   
-            WHERE a.owner_id = :owner_id';  
-                  
+        $sql = 'SELECT u.id, u.login
+            FROM users u
+            JOIN access a ON u.id = a.granted_to_id
+            WHERE a.owner_id = :owner_id';
+               
     $stmt = self::$db->prepare($sql);
     $stmt->execute(['owner_id' => $ownerId]);
     return $stmt->fetchAll();
