11de81f98747aa13763afa9006545ff3960aed7a
firefirefile <withsofatv@gmail.com>
2026-02-25 15:15:52 +0500

    change deleted_at parametr - now it is a is_deleted

--- src/Models/Book.php
+++ src/Models/Book.php
@@ -10,27 +10,47 @@ class Book extends Model {
      * запрашивает книги конкретного пользователя для списка моих книг 
      */
     public static function findByUserId(int $userId):array {
-        $sql = "SELECT * FROM " . static::$table . 
-        ' WHERE user_id = :user_id AND deleted_at IS NULL';
+        $sql = "SELECT * FROM " . static::$table .
+        ' WHERE user_id = :user_id AND is_deleted = FALSE';
         $stmt = self::$db->prepare($sql);
         $stmt->execute(['user_id' => $userId]);
-        return $stmt->fetchAll(); 
+        return $stmt->fetchAll();
     }
         /**
          * поиск книги по названию
          */
     public static function searchByTitle(string $query, int $userId):array {
-        $sql = "SELECT * FROM " . static::$table . " 
-        WHERE user_id = :user_id 
-        AND title LIKE :query 
-        AND deleted_at IS NULL";
+        $sql = "SELECT * FROM " . static::$table . "
+        WHERE user_id = :user_id
+        AND title LIKE :query
+        AND is_deleted = FALSE";
         $stmt = self::$db->prepare($sql);
         $stmt->execute([
             'user_id' => $userId,
             'query' => "%$query%"
             ]);
         return $stmt->fetchAll();
-    } 
+    }
+
+    /**
+     * удаление 
+     */
+    public static function softDelete(int $id):bool {
+        $sql = "UPDATE " . static::$table . " SET is_deleted = TRUE WHERE id = :id";
+        $stmt = self::$db->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->rowCount() > 0;
+    }
+
+    /**
+     * восстановление - меняет is_deleted обратно на false 
+     */
+    public static function restore(int $id):bool {
+        $sql = "UPDATE " . static::$table . " SET is_deleted = FALSE WHERE id = :id";
+        $stmt = self::$db->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->rowCount() > 0;
+    }
 
 
 
