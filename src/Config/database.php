81e6bdd954a48b109deace32f0c7fa555bc7778b
firefirefile <withsofatv@gmail.com>
2026-02-25 15:53:54 +0500

    chore: remove sensitive database config, add example file

--- src/Config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'host' => 'mysql',              // имя сервиса в docker-compose
-    'dbname' => 'library',          // имя базы данных
-    'user' => 'library_user',       // пользователь MySQL
-    'password' => 'library_pass',   // пароль
-    'charset' => 'utf8mb4'          // кодировка
-];
